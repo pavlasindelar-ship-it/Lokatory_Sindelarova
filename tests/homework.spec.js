fcd921909562263af0ffeaf8b1e9fb05e8efc3ba
Pavla_Sindelarova <pavla.sindelar@gmail.com>
2025-11-14 18:27:19 +0100

    Update homework.spec.js
    14.11.

--- tests/homework.spec.js
+++ tests/homework.spec.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { test, expect } from '@playwright/test';
+import {RegistrationPage} from './RegistrationPage.js';
 
 const {
     USER_USERNAME,
@@ -10,40 +11,56 @@ const {
     EMAIL_DOMAIN,
 }= process.env
 
-async function fillRegistrationForm(page, name, email, userPassword, confirmPassword) {
-    await page.locator("input#name").fill(name);
-    await page.locator("input#email").fill(email);
-    await page.locator("input#password").fill(userPassword);
-    await page.locator("input#password-confirm").fill(confirmPassword);
-    await page.locator(".btn-primary").click();
-}
-
 function createNewEmail() {
     return EMAIL_WITHOUT_DOMAIN + Date.now() + EMAIL_DOMAIN;
 }
 
 test.describe("login page", () => {
+
+    let registrationPageObject;
+    
     test.beforeEach(async ({ page }) => {
-        await page.goto("https://team8-2022brno.herokuapp.com/registrace");
+        registrationPageObject = new RegistrationPage(page);
+        registrationPageObject.goto();
     }); 
 
     test("valid registration", { tag: ["@happyway", "@valid"] }, async ({ page }) => {
-        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), USER_PASSWORD, USER_PASSWORD);
+        await registrationPageObject.register({
+          name: USER_USERNAME,
+          email: createNewEmail(), 
+          userPassword: USER_PASSWORD, 
+          confirmPassword: USER_PASSWORD
+        });
         await expect(page).toHaveURL("https://team8-2022brno.herokuapp.com/zaci");
     });
 
     test("invalid registration with existing mail", { tag: "@invalid" }, async ({ page }) => {
-        await fillRegistrationForm(page, USER_USERNAME, USER_EMAIL, USER_PASSWORD, USER_PASSWORD);
+        await registrationPageObject.register({
+          name: USER_USERNAME,
+          email: USER_EMAIL, 
+          userPassword: USER_PASSWORD, 
+          confirmPassword: USER_PASSWORD
+        });
         await expect(page.locator("text=Účet s tímto emailem již existuje")).toBeVisible();
     });
 
     test("invalid registration with incorrect password", { tag: "@invalid" }, async ({ page }) => {
-        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), INVALID_USER_PASSWORD, INVALID_USER_PASSWORD);
+        await registrationPageObject.register({
+          name: USER_USERNAME,
+          email: createNewEmail(), 
+          userPassword: INVALID_USER_PASSWORD, 
+          confirmPassword: INVALID_USER_PASSWORD
+        });
         await expect(page.locator("text=Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici")).toBeVisible();
     });
     
     test("invalid registration with incorrect confirmation", { tag: "@invalid" }, async ({ page }) => {
-        await fillRegistrationForm(page, USER_USERNAME, createNewEmail(), USER_PASSWORD, INVALID_USER_PASSWORD);
+        await registrationPageObject.register({
+          name: USER_USERNAME,
+          email: createNewEmail(), 
+          userPassword: USER_PASSWORD,
+          confirmPassword: INVALID_USER_PASSWORD
+        });
         await expect(page.locator("text=Hesla se neshodují")).toBeVisible();
     });
 
